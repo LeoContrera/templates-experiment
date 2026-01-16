178049d6b01f9e368fbe83c2d2c0b63c3b50772f
Aaron_Dev <aaronnunez170@gmail.com>
2026-01-16 17:48:41 -0300

    hola devuelta

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.generic import TemplateView
+
+import app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Ruta para ver el diseño base directamente (el experimento visual)
+    path('', TemplateView.as_view(template_name='base.html'), name='ver_base'),
     
+
+
 ]
